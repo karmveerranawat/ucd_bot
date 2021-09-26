04d7945d41ef387ba9088fae6017359bf2b20c48
karmveerranawat <karmveerranawat@gmail.com>
2021-09-26 23:27:12 +0530

    git push

--- commands/notion.js
+++ commands/notion.js
@@ -1,5 +1,7 @@
 const { Client } = require("@notionhq/client")
+const axios = require('axios');
 const dotenv = require("dotenv")
+
 dotenv.config()
 
 const webhook_url=process.env.DISCORD_WEBHOOK_URL;
@@ -7,16 +9,17 @@ const notion = new Client({
     auth: process.env.NOTION_KEY 
 })
 
+let trigger  = 0;
 let prevTodos = [
     {
         id: "1625f81b-78cf-49bf-8046-62bbbe3bd6d7",
         text: "Offer Letter",
-        checked: "false"
+        checked: true
     },
     {
         id: "68848740-4407-4fde-a69e-44ce534f9755",
-        text: "Welcome Freshers Reel and Poster",
-        checked: "false"
+        text: "Welcome Freshers Reel and Poste",
+        checked: false
     }
 ];
 
@@ -32,7 +35,6 @@ module.exports = {
 	name: 'notion',
 	description: 'Notion Api',
 	execute(message, args, Discord) {
-
         const exampleEmbed = new Discord.MessageEmbed();
         exampleEmbed.setColor('#0099ff')
         .setTitle('Notion Todos')
@@ -45,7 +47,7 @@ module.exports = {
                     return [
                         ...acc,
                         {
-                            type: "Status Update",
+                            type: "ToDo Title Update",
                             prevTitle: currentTodo.text,
                             newTitle: latesTodo.text
                         }
@@ -55,8 +57,58 @@ module.exports = {
                 }
             }, []);
 
+            const statusChange = prev.reduce((acc, currentTodo) => {
+                const latesTodo = curr.find(item => item.id === currentTodo.id)
+                if(latesTodo.checked !== currentTodo.checked){
+                    return [
+                        ...acc,
+                        {
+                            type: "ToDo Status Update",
+                            title: currentTodo.text,
+                            prevStatus: currentTodo.checked,
+                            newStatus: latesTodo.checked
+                        }
+                    ]
+                } else {
+                    return acc;
+                }
+            }, []);
+
+            const delTodo = prev.reduce((acc, currentTodo) => {
+                if(!curr.find(item => item.id === currentTodo.id)){
+                    return [
+                        ...acc,
+                        {
+                            type: "ToDo Delete Update",
+                            title: currentTodo.text,
+                            status: currentTodo.status
+                        }
+                    ]
+                } else {
+                    return acc;
+                }
+            }, []);
+
+            const todoCreate = curr.reduce((acc, currentTodo) => {
+                if(!prev.find(item => item.id === currentTodo.id)){
+                    return [
+                        ...acc,
+                        {
+                            type: "ToDo Created Update",
+                            newTitle: currentTodo.text,
+                            newStatus: currentTodo.status
+                        }
+                    ]
+                } else {
+                    return acc;
+                }
+            }, []);
+
             return {
-                titleChange
+                titleChange,
+                statusChange,
+                delTodo,
+                todoCreate
             };
         };
 
@@ -88,15 +140,101 @@ module.exports = {
                 // exampleEmbed.addField('Count', element.count);
             });
 
-            if(prevTodos){
+            if(trigger  == 0){
+                trigger = 1;
+                prevTodos=todos;
+            } else {
                 const diff = differ(prevTodos, todos);
+                if(diff.titleChange.length){
+                    axios.post(process.env.DISCORD_WEBHOOK_URL,
+                    {
+                        embeds: [{
+                            "title": diff.titleChange[0].type,
+                            "color": "14177041",
+                            "description": "",
+                            "fields": [
+                                {
+                                    name: "Previous Title : ",
+                                    value: "```"+diff.titleChange[0].prevTitle+"```"
+                                },
+                                {
+                                    name: "New Title : ",
+                                    value: "```"+diff.titleChange[0].newTitle+"```"
+                                }
+                            ]
+                        }]
+                    },
+                    {
+                        Headers: {
+                            'Content-Type': 'application/json'
+                        },
+                    })
+                }
+
+                if(diff.statusChange.length){
+                    axios.post(process.env.DISCORD_WEBHOOK_URL,
+                    {
+                        embeds: [{
+                            "title": diff.statusChange[0].type,
+                            "color": "1127128",
+                            "description": "```"+diff.statusChange[0].title+"```",
+                            "fields": [
+                                {
+                                    name: "Previous Status",
+                                    value: "```"+diff.statusChange[0].prevStatus+"```"
+                                },
+                                {
+                                    name: "New Status",
+                                    value: "```"+diff.statusChange[0].newStatus+"```"
+                                }                                
+                        ]
+                        }]
+                    },
+                    {
+                        Headers: {
+                            'Content-Type': 'application/json'
+                        },
+                    })
+                }
+
+                // if(diff.titleChange.length){
+                //     axios.post(process.env.DISCORD_WEBHOOK_URL,
+                //     {
+                //         embeds: [{
+                //             "title": diff.titleChange[0].type,
+                //             "description": "```"+"Previous Title: "+diff.titleChange[0].prevTitle+" \n"+"New Title: "+diff.titleChange[0].newTitle+"```",
+                //             "Color": "green"
+                //         }]
+                //     },
+                //     {
+                //         Headers: {
+                //             'Content-Type': 'application/json'
+                //         },
+                //     })
+                // }
+
+                // if(diff.titleChange.length){
+                //     axios.post(process.env.DISCORD_WEBHOOK_URL,
+                //     {
+                //         embeds: [{
+                //             "title": diff.titleChange[0].type,
+                //             "description": "```"+"Previous Title: "+diff.titleChange[0].prevTitle+" \n"+"New Title: "+diff.titleChange[0].newTitle+"```",
+                //             "Color": "green"
+                //         }]
+                //     },
+                //     {
+                //         Headers: {
+                //             'Content-Type': 'application/json'
+                //         },
+                //     })
+                // }
                 console.log(diff);
+                
                 prevTodos=todos;
             }
             //message.channel.send(JSON.stringify(allBlocks.results.filter(b => b.type === "to_do")));
             //message.channel.send(JSON.stringify(todos));
-            message.channel.send(exampleEmbed);
-            console.log(todos);
+            message.channel.send("cmd ran noicely");
         })()
 
 
