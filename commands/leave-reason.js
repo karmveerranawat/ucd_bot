9635b0de8c537c74688977aa049cd85c86661e9e
karmveerranawat <karmveerranawat@gmail.com>
2021-09-28 23:07:48 +0530

    Try 3 for making adapting heroku

--- commands/leave-reason.js
+++ commands/leave-reason.js
@@ -1,25 +1,25 @@
-const { save } = require('../firestoreClient');
-const firestoreClient = require('../firestoreClient');
-const FirestoreClient = require('../firestoreClient');
+// const { save } = require('../firestoreClient');
+// const firestoreClient = require('../firestoreClient');
+// const FirestoreClient = require('../firestoreClient');
 
-module.exports = {
-	name: 'leave-reason',
-	description: 'Provide Reason for not attending meeting',
-	execute(message, args) {
-        const record = {
-            name: message.author.username,
-            reason: args.toString(),
-            meetingDate: '01-01-01'
-        };
+// module.exports = {
+// 	name: 'leave-reason',
+// 	description: 'Provide Reason for not attending meeting',
+// 	execute(message, args) {
+//         const record = {
+//             name: message.author.username,
+//             reason: args.toString(),
+//             meetingDate: '01-01-01'
+//         };
 
-        const saveSubCollection = async() => {
-            await FirestoreClient.saveSubCollection('leave-reason', record.meetingDate, 'record', record);
-        }
-        saveSubCollection();
-        // const save = async() => {
-        //     await FirestoreClient.save('leave-reason', record);
-        // }
-        // save();
-        message.channel.send(`${message.author} Leave Reason Recorded`);
-	},
-};
+//         const saveSubCollection = async() => {
+//             await FirestoreClient.saveSubCollection('leave-reason', record.meetingDate, 'record', record);
+//         }
+//         saveSubCollection();
+//         // const save = async() => {
+//         //     await FirestoreClient.save('leave-reason', record);
+//         // }
+//         // save();
+//         message.channel.send(`${message.author} Leave Reason Recorded`);
+// 	},
+// };
