544ae259d7549341b92064a393294f4c00191de7
karmveerranawat <karmveerranawat@gmail.com>
2021-10-03 18:14:28 +0530

    automated runtime script trigger

--- commands/notion.js
+++ commands/notion.js
@@ -1,251 +1,251 @@
-const { Client } = require("@notionhq/client")
-const axios = require('axios');
-const dotenv = require("dotenv")
+const { Client } = require("@notionhq/client");
+const axios = require("axios");
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
-const webhook_url=process.env.DISCORD_WEBHOOK_URL;
-const notion = new Client({ 
-    auth: process.env.NOTION_KEY 
-})
+const webhook_url = process.env.DISCORD_WEBHOOK_URL;
+const notion = new Client({
+  auth: process.env.NOTION_KEY,
+});
 
-let trigger  = 0;
+let trigger = 0;
 let time = 30000;
 let prevTodos = [
-    {
-        id: "1625f81b-78cf-49bf-8046-62bbbe3bd6d7",
-        text: "Offer Letter",
-        checked: true
-    },
-    {
-        id: "68848740-4407-4fde-a69e-44ce534f9755",
-        text: "Welcome Freshers Reel and Poste",
-        checked: false
-    }
+  {
+    id: "1625f81b-78cf-49bf-8046-62bbbe3bd6d7",
+    text: "Offer Letter",
+    checked: true,
+  },
+  {
+    id: "68848740-4407-4fde-a69e-44ce534f9755",
+    text: "Welcome Freshers Reel and Poste",
+    checked: false,
+  },
 ];
 
 const blockPayloadGen = {
-    path: "blocks/874ca5ac-2fbc-4dbf-9c05-828535214764/children",
-    method: "GET",
-    body: {
-       
-    }
+  path: "blocks/874ca5ac-2fbc-4dbf-9c05-828535214764/children",
+  method: "GET",
+  body: {},
 };
 
 module.exports = {
-	name: 'notion',
-	description: 'Notion Api',
-	execute(message, args, Discord) {
-        message.channel.send("Notion Pulls Have been started Succesfully , it will run every 2 minutes");
-        setInterval(() => {
-            const exampleEmbed = new Discord.MessageEmbed();
-            exampleEmbed.setColor('#0099ff')
-            .setTitle('Notion Todos')
-            .setURL('https://ucdupes.org/');
+  name: "notion",
+  description: "Notion Api",
+  execute(Discord) {
+    console.log(
+      "Notion Pulls Have been started Succesfully , it will run every 30 seconds"
+    );
+    setInterval(() => {
+      const exampleEmbed = new Discord.MessageEmbed();
+      exampleEmbed
+        .setColor("#0099ff")
+        .setTitle("Notion Todos")
+        .setURL("https://ucdupes.org/");
 
-            
+      const differ = (prev, curr) => {
+        const delTodo = prev.reduce((acc, currentTodo) => {
+          if (!curr.find((item) => item.id === currentTodo.id)) {
+            return [
+              ...acc,
+              {
+                title: "ToDo Delete Update",
+                color: "1127128",
+                description: "",
+                fields: [
+                  {
+                    name: "~~Deleted Todo's Title~~",
+                    value: "~~```" + currentTodo.text + "```~~",
+                  },
+                  {
+                    name: "~~Deleted Todo's Status~~",
+                    value: "~~```" + currentTodo.status + "```~~",
+                  },
+                ],
+              },
+            ];
+          } else {
+            return acc;
+          }
+        }, []);
 
-            const differ = (prev, curr) => {
+        const todoCreate = curr.reduce((acc, currentTodo) => {
+          if (!prev.find((item) => item.id === currentTodo.id)) {
+            return [
+              ...acc,
+              {
+                title: "New ToDo Added",
+                color: "1127128",
+                description: "",
+                fields: [
+                  {
+                    name: "New Todo's Title",
+                    value: "```" + currentTodo.text + "```",
+                  },
+                  {
+                    name: "New Todo's Status",
+                    value: "```" + currentTodo.status + "```",
+                  },
+                ],
+              },
+            ];
+          } else {
+            return acc;
+          }
+        }, []);
 
-                const delTodo = prev.reduce((acc, currentTodo) => {
-                    if(!curr.find(item => item.id === currentTodo.id)){
-                        return [
-                            ...acc,
-                            {
-                                "title": "ToDo Delete Update",
-                                "color": "1127128",
-                                "description": "",
-                                "fields": [
-                                    {
-                                        name: "~~Deleted Todo's Title~~",
-                                        value: "~~```"+currentTodo.text+"```~~"
-                                    },
-                                    {
-                                        name: "~~Deleted Todo's Status~~",
-                                        value: "~~```"+currentTodo.status+"```~~"
-                                    }                                
-                                ]
-                            }
-                        ]
-                    } else {
-                        return acc;
-                    }
-                }, []);
+        const titleChange = prev.reduce((acc, currentTodo) => {
+          const latesTodo = curr.find((item) => item.id === currentTodo.id);
+          if (latesTodo == undefined) {
+            return acc;
+          } else if (latesTodo.text !== currentTodo.text) {
+            return [
+              ...acc,
+              {
+                title: "ToDo Title Update",
+                color: "14177041",
+                description: "",
+                fields: [
+                  {
+                    name: "Previous Title : ",
+                    value: "```" + currentTodo.text + "```",
+                  },
+                  {
+                    name: "New Title : ",
+                    value: "```" + latesTodo.text + "```",
+                  },
+                ],
+              },
+            ];
+          } else {
+            return acc;
+          }
+        }, []);
 
-                const todoCreate = curr.reduce((acc, currentTodo) => {
-                    if(!prev.find(item => item.id === currentTodo.id)){
-                        return [
-                            ...acc,
-                            {
-                                "title": "New ToDo Added",
-                                "color": "1127128",
-                                "description": "",
-                                "fields": [
-                                    {
-                                        name: "New Todo's Title",
-                                        value: "```"+currentTodo.text+"```"
-                                    },
-                                    {
-                                        name: "New Todo's Status",
-                                        value: "```"+currentTodo.status+"```"
-                                    }                                
-                                ]
-                            }
-                        ]
-                    } else {
-                        return acc;
-                    }
-                }, []);
+        const statusChange = prev.reduce((acc, currentTodo) => {
+          const latesTodo = curr.find((item) => item.id === currentTodo.id);
+          if (latesTodo == undefined) {
+            return acc;
+          } else if (latesTodo.checked !== currentTodo.checked) {
+            return [
+              ...acc,
+              {
+                title: "ToDo Status Update",
+                color: "1127128",
+                description: "```" + currentTodo.text + "```",
+                fields: [
+                  {
+                    name: "Previous Status",
+                    value: "```" + currentTodo.checked + "```",
+                  },
+                  {
+                    name: "New Status",
+                    value: "```" + latesTodo.checked + "```",
+                  },
+                ],
+              },
+            ];
+          } else {
+            return acc;
+          }
+        }, []);
 
-                const titleChange = prev.reduce((acc, currentTodo) => {
-                    const latesTodo = curr.find(item => item.id === currentTodo.id)
-                    if(latesTodo == undefined){
-                        return acc;
-                    }
-                    else if(latesTodo.text !== currentTodo.text){
-                        return [
-                            ...acc,
-                            {                              
-                                "title": "ToDo Title Update",
-                                "color": "14177041",
-                                "description": "",
-                                "fields": [
-                                    {
-                                        name: "Previous Title : ",
-                                        value: "```"+currentTodo.text+"```"
-                                    },
-                                    {
-                                        name: "New Title : ",
-                                        value: "```"+latesTodo.text+"```"
-                                    }
-                                ]
-                                
-                            }
-                        ]
-                    } else {
-                        return acc;
-                    }
-                }, []);
+        return {
+          titleChange,
+          statusChange,
+          delTodo,
+          todoCreate,
+        };
+      };
 
-                const statusChange = prev.reduce((acc, currentTodo) => {
-                    const latesTodo = curr.find(item => item.id === currentTodo.id)
-                    if(latesTodo == undefined){
-                        return acc;
-                    }
-                    else if(latesTodo.checked !== currentTodo.checked){
-                        return [
-                            ...acc,
-                            {
-                                "title": "ToDo Status Update",
-                                "color": "1127128",
-                                "description": "```"+currentTodo.text+"```",
-                                "fields": [
-                                    {
-                                        name: "Previous Status",
-                                        value: "```"+currentTodo.checked+"```"
-                                    },
-                                    {
-                                        name: "New Status",
-                                        value: "```"+latesTodo.checked+"```"
-                                    }                                
-                                ]
-                            }
-                        ]
-                    } else {
-                        return acc;
-                    }
-                }, []);
+      (async () => {
+        const allBlocks = await notion.request(blockPayloadGen);
+        const todos = allBlocks.results
+          .filter((b) => b.type === "to_do")
+          .map((todo) => ({
+            text: todo.to_do.text[0].plain_text,
+            checked: todo.to_do.checked,
+            id: todo.id,
+          }));
 
-                return {
-                    titleChange,
-                    statusChange,
-                    delTodo,
-                    todoCreate
-                };
-            };
+        // todos.forEach(element => {
+        //     exampleEmbed.addFields(
+        //         {
+        //             name: 'Task ID '+element.id,
+        //             value: "```"+element.text+" \n"+element.checked+"```"+emote
+        //         }
+        //     )
+        //     // exampleEmbed.addField('Refferal', element.refferal,true);
+        //     // exampleEmbed.addField('Count', element.count);
+        // });
 
-            (async() => {
-                const allBlocks = await notion.request(blockPayloadGen);
-                const todos = allBlocks.results
-                .filter(b => b.type === "to_do")
-                .map(todo => ({
-                    text: todo.to_do.text[0].plain_text,
-                    checked: todo.to_do.checked,
-                    id: todo.id
-                }));
+        if (trigger == 0) {
+          trigger = 1;
+          prevTodos = todos;
+        } else {
+          const diff = differ(prevTodos, todos);
+          if (diff.titleChange.length) {
+            axios.post(
+              process.env.DISCORD_WEBHOOK_URL,
+              {
+                embeds: diff.titleChange,
+              },
+              {
+                Headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+          }
 
-                // todos.forEach(element => {
-                //     exampleEmbed.addFields(
-                //         { 
-                //             name: 'Task ID '+element.id,
-                //             value: "```"+element.text+" \n"+element.checked+"```"+emote
-                //         }
-                //     )
-                //     // exampleEmbed.addField('Refferal', element.refferal,true);
-                //     // exampleEmbed.addField('Count', element.count);
-                // });
+          if (diff.statusChange.length) {
+            axios.post(
+              process.env.DISCORD_WEBHOOK_URL,
+              {
+                embeds: diff.statusChange,
+              },
+              {
+                Headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+          }
 
-                if(trigger  == 0){
-                    trigger = 1;
-                    prevTodos=todos;
-                    
-                } else {
-                    const diff = differ(prevTodos, todos);
-                    if(diff.titleChange.length){
-                        
-                        axios.post(process.env.DISCORD_WEBHOOK_URL,
-                        {
-                            embeds: diff.titleChange
-                        },
-                        {
-                            Headers: {
-                                'Content-Type': 'application/json'
-                            },
-                        })
-                    }
+          if (diff.delTodo.length) {
+            axios.post(
+              process.env.DISCORD_WEBHOOK_URL,
+              {
+                embeds: diff.delTodo,
+              },
+              {
+                Headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+          }
 
-                    if(diff.statusChange.length){
-                        axios.post(process.env.DISCORD_WEBHOOK_URL,
-                        {
-                            embeds: diff.statusChange
-                        },
-                        {
-                            Headers: {
-                                'Content-Type': 'application/json'
-                            },
-                        })
-                    }
-
-                    if(diff.delTodo.length){
-                        axios.post(process.env.DISCORD_WEBHOOK_URL,
-                        {
-                            embeds: diff.delTodo
-                        },
-                        {
-                            Headers: {
-                                'Content-Type': 'application/json'
-                            },
-                        })
-                    }
-
-                    if(diff.todoCreate.length){
-                        axios.post(process.env.DISCORD_WEBHOOK_URL,
-                        {
-                            embeds: diff.todoCreate
-                        },
-                        {
-                            Headers: {
-                                'Content-Type': 'application/json'
-                            },
-                        })
-                    }
-                    console.log(diff);
-                    prevTodos=todos;
-                }
-                //message.channel.send(JSON.stringify(allBlocks.results.filter(b => b.type === "to_do")));
-                //message.channel.send(JSON.stringify(todos));
-            })()
-        }, time);
-        
-    },
-};        
+          if (diff.todoCreate.length) {
+            axios.post(
+              process.env.DISCORD_WEBHOOK_URL,
+              {
+                embeds: diff.todoCreate,
+              },
+              {
+                Headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+          }
+          console.log(diff);
+          prevTodos = todos;
+        }
+        //message.channel.send(JSON.stringify(allBlocks.results.filter(b => b.type === "to_do")));
+        //message.channel.send(JSON.stringify(todos));
+      })();
+    }, time);
+  },
+};
--- main.js
+++ main.js
@@ -1,127 +1,129 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 //const {token} = require('./config.json');
-const dotenv = require("dotenv")
-const fs = require('fs');
-const Imap = require('node-imap')
-const inspect = require('util').inspect;
-const { resolve } = require('path');
-const { exit } = require('process');
-const prefix = '.';
+const dotenv = require("dotenv");
+const fs = require("fs");
+const Imap = require("node-imap");
+const inspect = require("util").inspect;
+const { resolve } = require("path");
+const { exit } = require("process");
+const prefix = ".";
 const status = require("./globals/cmd_status_check");
-const { channel } = require('diagnostics_channel');
+const { channel } = require("diagnostics_channel");
 
-dotenv.config()
+dotenv.config();
 const token = process.env.TOKEN;
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 var temp_cmds;
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+  const command = require(`./commands/${file}`);
 
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-    console.log('UCD bot online');
-    client.user.setPresence({
-        activity: {
-            name: "UPCOMING TECH TALK",
-            type: "WATCHING"
-        },
-        status: "online"
-    });
-    
+client.once("ready", () => {
+  client.commands.get("notion").execute(Discord);
+  console.log("UCD bot online");
+  client.user.setPresence({
+    activity: {
+      name: "UPCOMING TECH TALK",
+      type: "WATCHING",
+    },
+    status: "online",
+  });
 });
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-    if(command === 'ping'){
-        if (!status(command)) return message.channel.send(`<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `);
-        client.commands.get('ping').execute(message, args);
-    } 
-
-    else if(command === 'leave-reason'){
-        if (!args.length) return message.channel.send(`Please provide a reason for your leave, ${message.author}!`);
-        client.commands.get('leave-reason').execute(message, args);
-    } 
-
-    else if(command === 'count' ||command === 'c'){
-        temp_cmds="count"
-        if(status(temp_cmds)) return message.channel.send(`<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `);
-        client.commands.get('count').execute(message, args);
-        
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (command === "ping") {
+    if (!status(command))
+      return message.channel.send(
+        `<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `
+      );
+    client.commands.get("ping").execute(message, args);
+  } else if (command === "leave-reason") {
+    if (!args.length)
+      return message.channel.send(
+        `Please provide a reason for your leave, ${message.author}!`
+      );
+    client.commands.get("leave-reason").execute(message, args);
+  } else if (command === "count" || command === "c") {
+    temp_cmds = "count";
+    if (status(temp_cmds))
+      return message.channel.send(
+        `<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `
+      );
+    client.commands.get("count").execute(message, args);
+  } else if (command === "ref") {
+    if (status(command) == true)
+      return message.channel.send(
+        `<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `
+      );
+    client.commands.get("ref").execute(message, args, Discord);
+  } else if (command === "send-mail") {
+    client.commands.get("send-mail").execute(message, args);
+  } else if (command === "mark") {
+    if (status(command) == true)
+      return message.channel.send(
+        `<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `
+      );
+    client.commands.get("mark").execute(message, args);
+  } else if (command === "update") {
+    client.commands.get("update").execute(message, args);
+  } else if (command === "join") {
+    // client.commands.get('update').execute(message, args);
+    message.channel.send("soon <:ucd_evil_racoon:845339340024578069>");
+  } else if (command === "disable") {
+    if (!message.member.roles.cache.has("844600000461012993"))
+      return message.channel.send(
+        `This is Admin only command <:ucd_badelog:856192103629062185> `
+      );
+    if (!args.length)
+      return message.channel.send(
+        `<:ucd_saste_nashe:845329317071683644> Atleast specify what to disable, ${message.author}! `
+      );
+    client.commands.get("disable").execute(message, args);
+  } else if (command === "enable") {
+    if (!message.member.roles.cache.has("844600000461012993"))
+      return message.channel.send(
+        `This is Admin only command <:ucd_badelog:856192103629062185> `
+      );
+    if (!args.length)
+      return message.channel.send(
+        `<:ucd_saste_nashe:845329317071683644> Atleast specify what to enable, ${message.author}! `
+      );
+    client.commands.get("enable").execute(message, args);
+  } else if (command === "notion") {
+    // if(!message.member.roles.cache.has("844600000461012993")) return message.channel.send(`This is Admin only command <:ucd_badelog:856192103629062185> `);
+    // if (!args.length) return message.channel.send(`<:ucd_saste_nashe:845329317071683644> Atleast specify what to enable, ${message.author}! `);
+    client.commands.get("notion").execute(message, args, Discord);
+  } else if (command === "discord") {
+    message.channel.send("UCD DISCORD LINK : https://ucdupes.org/discord");
+  } else if (command === "shutdown") {
+    if (!message.member.roles.cache.has("844600000461012993"))
+      return message.channel.send(
+        `This is Admin only command <:ucd_badelog:856192103629062185> `
+      );
+    timeinMS = 500;
+    delay(timeinMS, exit());
+    async function delay(timeinMS) {
+      await new Promise((resolve) => setTimeout(resolve, timeinMS));
     }
-
-    else if(command === 'ref'){
-        if(status(command) == true) return message.channel.send(`<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `);
-        client.commands.get('ref').execute(message, args, Discord);
-    }
-
-    else if(command === 'send-mail'){
-        client.commands.get('send-mail').execute(message, args);
-    } 
-
-    else if(command === 'mark'){
-        if(status(command) == true) return message.channel.send(`<:ucd_rage:872884631328948234> This command is disabled in server, ${message.author}! `);
-        client.commands.get('mark').execute(message, args);
-    } 
-
-    else if(command === 'update'){
-        client.commands.get('update').execute(message, args);
-    }
-
-    else if(command === 'join'){
-        // client.commands.get('update').execute(message, args);
-        message.channel.send("soon <:ucd_evil_racoon:845339340024578069>");
-    }
-
-    else if(command === 'disable'){
-        if(!message.member.roles.cache.has("844600000461012993")) return message.channel.send(`This is Admin only command <:ucd_badelog:856192103629062185> `);
-        if (!args.length) return message.channel.send(`<:ucd_saste_nashe:845329317071683644> Atleast specify what to disable, ${message.author}! `);
-        client.commands.get('disable').execute(message, args);
-    }
-
-    else if(command === 'enable'){
-        if(!message.member.roles.cache.has("844600000461012993")) return message.channel.send(`This is Admin only command <:ucd_badelog:856192103629062185> `);
-        if (!args.length) return message.channel.send(`<:ucd_saste_nashe:845329317071683644> Atleast specify what to enable, ${message.author}! `);
-        client.commands.get('enable').execute(message, args);
-    }
-
-    else if(command === 'notion'){
-        // if(!message.member.roles.cache.has("844600000461012993")) return message.channel.send(`This is Admin only command <:ucd_badelog:856192103629062185> `);
-        // if (!args.length) return message.channel.send(`<:ucd_saste_nashe:845329317071683644> Atleast specify what to enable, ${message.author}! `);
-        client.commands.get('notion').execute(message, args, Discord);
-    }
-
-    else if(command === 'discord'){
-        message.channel.send("UCD DISCORD LINK : https://ucdupes.org/discord");
-    }
-
-    else if (command === 'shutdown'){
-        if(!message.member.roles.cache.has("844600000461012993")) return message.channel.send(`This is Admin only command <:ucd_badelog:856192103629062185> `);
-        timeinMS=500
-        delay(timeinMS,exit())
-        async function delay(timeinMS){
-            await new Promise(resolve => setTimeout(resolve, timeinMS))
-        }  
-        message.channel.send("done! will there be anything else ?");
-          
-    }
-
-    else if (command === 'play'){
-        client.commands.get('play').execute(message, args);
-    }
-
-    else if (command === 'leave'){
-        client.commands.get('leave').execute(message, args);
-    }
+    message.channel.send("done! will there be anything else ?");
+  } else if (command === "play") {
+    client.commands.get("play").execute(message, args);
+  } else if (command === "leave") {
+    client.commands.get("leave").execute(message, args);
+  }
 });
 
 // const OLDMAILS = new Map();
@@ -158,6 +160,5 @@ client.on('message', message => {
 //         }, delayInMS)
 //     })
 // }
-
 
 client.login(token);
